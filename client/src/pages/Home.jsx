0575990a28ca8b9bd4538b1ec02dbb44eb9236aa
vinaydeep26 <vinaydeep26@gmail.com>
2023-05-11 16:28:19 -0400

    Changes to be committed: 	modified:   client/src/pages/CreatePost.jsx 	modified:   client/src/pages/Home.jsx 	modified:   server/routes/postRoutes.js
    Changes not staged for commit:
    	modified:   server/.env

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,32 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState('');
-
+    const fetchPosts = async () => {
+      setLoading(true);
+  
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+  
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    useEffect(() => {
+      fetchPosts();
+    }, []);
+  
   return (
     <section className="max-w-7xl mx-auto">
         <div>
